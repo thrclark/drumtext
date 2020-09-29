e11f1b68669748ad08bb096cba7dd9f3ed1d5c4c
thrclark <thrclark@users.noreply.github.com>
2020-09-29 16:45:46 -0400

    setup

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # drumtext
+Drumtext website
---
+++ php/signup.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="">
+<title>Title</title>
+
+<!-- Custom Fonts -->
+
+<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Dosis:200,300|Rasa&display=swap" rel="stylesheet">
+
+
+<!-- Custom CSS -->
+<link href="../css/main.css" rel="stylesheet" type="text/css">
+</head>
+
+<body id="page-top">
+
+<!-- Navigation --> 
+<a class="menu-toggle rounded" href="#"> <i class="fa fa-bars"></i> </a>
+<nav id="sidebar-wrapper">
+    <ul class="sidebar-nav">
+        <li class="sidebar-brand"> <a class="js-scroll-trigger" href="#page-top">Nav 1</a> </li>
+        <li class="sidebar-nav-item"> <a class="js-scroll-trigger" href="#page-top">Nav 2</a> </li>
+        <li class="sidebar-nav-item"> <a class="js-scroll-trigger" href="#about">Nav 3</a> </li>
+
+    </ul>
+</nav>
+
+<!-- Header -->
+<header class="masthead d-flex">
+    <div class="container text-center my-auto text-light">
+        <h1 class="mb-1">DrumText</h1>
+        <div class="mb-5"> <em>jazz happens here</em> </div>
+        <a class="btn btn-outline-light js-scroll-trigger mt-5" href="#about">About</a> </div>
+    <div class="overlay"></div>
+</header>
+
+<!-- About -->
+<section class="content-section bg-light" id="about">
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-lg-10 mx-auto">
+                <h1 class="mb-5">About</h1>
+                <p class="mb-5">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,</p>
+                <a class="btn btn-dark js-scroll-trigger" href="#services">Services</a> </div>
+        </div>
+    </div>
+</section>
+
+
+
+<!-- Footer -->
+<footer class="footer text-center">
+    <div class="container">
+        <ul class="list-inline mb-5">
+            <li class="list-inline-item"> <a class="social-link rounded-circle text-white mr-3" href="#"> <i class="fa fa-facebook"></i> </a> </li>
+            <li class="list-inline-item"> <a class="social-link rounded-circle text-white mr-3" href="#"> <i class="fa fa-twitter"></i> </a> </li>
+            <li class="list-inline-item"> <a class="social-link rounded-circle text-white" href="#"> <i class="fa fa-instagram"></i> </a> </li>
+        </ul>
+        <p class="text-muted small mb-0">Copyright &copy; Title 2020</p>
+    </div>
+</footer>
+
+<!-- Scroll to Top Button--> 
+<a class="scroll-to-top rounded js-scroll-trigger" href="#page-top"> <i class="fa fa-angle-up"></i> </a> 
+
+<!-- Bootstrap core JavaScript --> 
+<script src="js/jquery.min.js"></script> 
+<script src="js/bootstrap.bundle.js"></script> 
+
+<!-- Plugin JavaScript --> 
+<script src="js/jquery.easing.min.js"></script> 
+
+<!-- Custom scripts for this template --> 
+<script src="js/stylish-portfolio.min.js"></script> 
+
+
+</body>
+</html>
+
+<!--'<div class="media text-muted pt-3">' + '<p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">' + '<strong class="d-block">'-->
