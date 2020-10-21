ccb8f509e0060f8f2674982f0028f35300673ccd
thrclark <thrclark@users.noreply.github.com>
2020-10-20 20:15:10 -0400

    updates

--- php/signup.php
+++ php/signup.php
@@ -24,9 +24,13 @@
 <a class="menu-toggle rounded" href="#"> <i class="fa fa-bars"></i> </a>
 <nav id="sidebar-wrapper">
     <ul class="sidebar-nav">
-        <li class="sidebar-brand"> <a class="js-scroll-trigger" href="#page-top">Nav 1</a> </li>
-        <li class="sidebar-nav-item"> <a class="js-scroll-trigger" href="#page-top">Nav 2</a> </li>
-        <li class="sidebar-nav-item"> <a class="js-scroll-trigger" href="#about">Nav 3</a> </li>
+        <li class="sidebar-brand"> <a class="js-scroll-trigger" href="#page-top">Drumtext</a> </li>
+        <li class="sidebar-nav-item"> <a class="js-scroll-trigger" href="#page-top">Sign up/in</a> </li>
+        <li class="sidebar-nav-item"> <a class="js-scroll-trigger" href="#about">About</a> </li>
+        
+         <li class="sidebar-nav-item"> <a class="js-scroll-trigger" href="#testimonials">Testimonials</a> </li>
+        
+        
 
     </ul>
 </nav>
@@ -35,8 +39,8 @@
 <header class="masthead d-flex">
     <div class="container text-center my-auto text-light">
         <h1 class="mb-1">DrumText</h1>
-        <div class="mb-5"> <em>jazz happens here</em> </div>
-        <a class="btn btn-outline-light js-scroll-trigger mt-5" href="#about">About</a> </div>
+        <div class="mb-5"> <em>music lessons from the masters</em> </div>
+        <a class="btn btn-primary mt-5" href="#">Sign in</a>  <a class="btn btn-secondary mt-5" href="#">Sign up</a> </div>
     <div class="overlay"></div>
 </header>
 
@@ -47,11 +51,44 @@
             <div class="col-lg-10 mx-auto">
                 <h1 class="mb-5">About</h1>
                 <p class="mb-5">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,</p>
-                <a class="btn btn-dark js-scroll-trigger" href="#services">Services</a> </div>
+             </div>
         </div>
     </div>
 </section>
 
+    
+    
+    
+<!-- Services -->
+<section class="content-section bg-primary text-white text-center" id="testimonials">
+    <div class="container">
+        <div class="content-section-heading">
+            <h2 class="mb-5">Testimonials</h2>
+        </div>
+        <div class="row">
+            <div class="col-lg-3 col-md-6 mb-5 mb-lg-0"> <span class="service-icon rounded-circle mx-auto mb-3"> <i class="icon-screen-smartphone"></i> </span>
+                <h4> <strong>John C</strong> </h4>
+                <p class="text-faded mb-0">"Aenean commodo ligula eget dolor"</p>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-5 mb-lg-0"> <span class="service-icon rounded-circle mx-auto mb-3"> <i class="icon-pencil"></i> </span>
+                <h4> <strong>Phil W</strong> </h4>
+                <p class="text-faded mb-0">"Maecenas nec odio et ante tincidunt tempus"</p>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-5 mb-md-0"> <span class="service-icon rounded-circle mx-auto mb-3"> <i class="icon-like"></i> </span>
+                <h4> <strong>Angela C</strong> </h4>
+                <p class="text-faded mb-0">"Nam quam nunc, blandit vel, luctus lorem"</p>
+            </div>
+            <div class="col-lg-3 col-md-6"> <span class="service-icon rounded-circle mx-auto mb-3"> <i class="icon-mustache"></i> </span>
+                <h4> <strong>Stan G</strong> </h4>
+                <p class="text-faded mb-0">"Etiam sit amet orci eget eros faucibus tincidunt"</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+    
+    
+    
 
 
 <!-- Footer -->
@@ -62,7 +99,7 @@
             <li class="list-inline-item"> <a class="social-link rounded-circle text-white mr-3" href="#"> <i class="fa fa-twitter"></i> </a> </li>
             <li class="list-inline-item"> <a class="social-link rounded-circle text-white" href="#"> <i class="fa fa-instagram"></i> </a> </li>
         </ul>
-        <p class="text-muted small mb-0">Copyright &copy; Title 2020</p>
+        <p class="text-muted small mb-0">Copyright &copy; Drumtext 2020</p>
     </div>
 </footer>
 
@@ -70,14 +107,14 @@
 <a class="scroll-to-top rounded js-scroll-trigger" href="#page-top"> <i class="fa fa-angle-up"></i> </a> 
 
 <!-- Bootstrap core JavaScript --> 
-<script src="js/jquery.min.js"></script> 
-<script src="js/bootstrap.bundle.js"></script> 
+<script src="../js/jquery.min.js"></script> 
+<script src="../js/bootstrap.bundle.js"></script> 
 
 <!-- Plugin JavaScript --> 
-<script src="js/jquery.easing.min.js"></script> 
+<script src="../js/jquery.easing.min.js"></script> 
 
 <!-- Custom scripts for this template --> 
-<script src="js/stylish-portfolio.min.js"></script> 
+<script src="../js/stylish-portfolio.min.js"></script> 
 
 
 </body>
